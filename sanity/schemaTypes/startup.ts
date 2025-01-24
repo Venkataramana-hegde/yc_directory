c8b3f648f0835a730288fc93ddb5300542fec1fc
Venkataramana hegde <venkataramanahegde64@gmail.com>
2025-01-24 22:45:36 +0530

    Add author sanity schema

---
+++ sanity/schemaTypes/startup.ts
@@ -0,0 +1,43 @@
+import { UserIcon } from "lucide-react";
+
+export const author = defineType({
+  name: "startup",
+  title: "Startup",
+  type: "document",
+  icon: UserIcon,
+  fields: [
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title"
+      }
+    }),
+    defineField({
+      name: "name",
+      type: "string",
+    }),
+    defineField({
+      name: "username",
+      type: "string",
+    }),
+    defineField({
+      name: "email",
+      type: "string",
+    }),
+    defineField({
+      name: "image",
+      type: "url",
+    }),
+    defineField({
+      name: "bio",
+      type: "text",
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: "name",
+    },
+  },
+});
